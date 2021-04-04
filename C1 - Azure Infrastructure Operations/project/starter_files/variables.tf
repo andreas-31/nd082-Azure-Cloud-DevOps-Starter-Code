d183673e19994c3b265a55c22f45cde312aee4eb
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-04 19:22:25 +0200

    Update variables.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -1,19 +1,7 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default = "FirstTerraform"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default = "East US"
-}
-
-variable "username" {
-  description = "Name of user for login"
-  default = "sportler"
-}
-
-variable "password" {
-  description = "Password of user for login"
-  default = "aZ8_99-WpQ4_8000_4000"
 }

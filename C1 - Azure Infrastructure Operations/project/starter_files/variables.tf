6001a0ce95a405fb16b41f383da170555f34bbf9
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-04 19:31:51 +0200

    Update variables.tf
    Added default values and new variable resource_group_name

--- C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -1,7 +1,14 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default     = "udacity-project1"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources are created"
+  default     = "udacity-cli-rg"
 }

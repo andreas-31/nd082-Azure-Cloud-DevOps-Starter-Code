235cd68a330ec9c550a4a65859a9eac6fda40512
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-04 19:52:29 +0200

    Update variables.tf
    Added variables application_port and admin_password

--- C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -12,3 +12,13 @@ variable "resource_group_name" {
   description = "The name of the resource group in which the resources are created"
   default     = "udacity-cli-rg"
 }
+
+variable "application_port" {
+    description = "The port that you want to expose to the external load balancer"
+    default     = 80
+}
+
+variable "admin_password" {
+    description = "Default password for admin"
+    default = "Passwwoord11223344"
+}

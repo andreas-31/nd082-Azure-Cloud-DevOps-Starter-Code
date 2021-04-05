89b40acfbc8e738bf15656a03a18461baaaeaf8c
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-05 17:17:13 +0200

    Update main.tf
    Changed allocation method of public IP from dynamic to static

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "${var.prefix}-pip"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   
   tags = {
     udacity = "Project Web Server"

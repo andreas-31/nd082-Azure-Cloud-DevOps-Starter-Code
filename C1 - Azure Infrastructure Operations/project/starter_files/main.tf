1eb1c388e5ae75862237215b9d1179dc4687a004
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-06 18:10:01 +0200

    chore: update main.tf
    Added hyphens to names of azurerm_availability_set and azurerm_network_security_group

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_availability_set" "avset" {
-  name                         = "${var.prefix}avset"
+  name                         = "${var.prefix}-avset"
   location                     = azurerm_resource_group.main.location
   resource_group_name          = azurerm_resource_group.main.name
   platform_fault_domain_count  = 2
@@ -74,7 +74,7 @@ resource "azurerm_availability_set" "avset" {
 }
 
 resource "azurerm_network_security_group" "webserver" {
-  name                = "${var.prefix}http_webserver-nsg"
+  name                = "${var.prefix}-http_webserver-nsg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   security_rule {

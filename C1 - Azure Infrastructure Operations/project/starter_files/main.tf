cc52ac929854d9595d5dce3abcc21e71c189093b
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-06 16:05:51 +0200

    Update main.tf
    Added azurerm_lb_probe for health check of load balancer

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -109,6 +109,13 @@ resource "azurerm_lb" "example" {
   }
 }
 
+resource "azurerm_lb_probe" "example" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.example.id
+  name                = "http-running-probe"
+  port                = 80
+}
+
 resource "azurerm_lb_backend_address_pool" "example" {
   resource_group_name = azurerm_resource_group.main.name
   loadbalancer_id     = azurerm_lb.example.id

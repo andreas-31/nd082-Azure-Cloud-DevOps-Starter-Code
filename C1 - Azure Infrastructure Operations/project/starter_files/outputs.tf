fac08ba6c8e128ad2cf035fd8a67b71088c4616f
Ubuntu <azureuser@UbuntuTerminal.aj2333tiwxculhtlowp4co5gtg.bx.internal.cloudapp.net>
2021-04-08 16:31:26 +0000

    Added private VM IPs to output; added Terraform solution.plan file

--- C1 - Azure Infrastructure Operations/project/starter_files/outputs.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/outputs.tf
@@ -1,4 +1,9 @@
-output "public_IP_LB" {
+output "public_IP_Load_Balancer" {
   value       = azurerm_public_ip.pip.ip_address
-  description = "The public IP address of the load balancer in front of the web servers."
+  description = "Public HTTP service IP address of the load balancer in front of the webservers."
+}
+
+output "network_Interface_Private_IP" {
+  description = "Private IP addresses of the VM network interfaces"
+  value       = azurerm_network_interface.main.*.private_ip_address
 }
